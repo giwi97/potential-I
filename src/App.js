0d86ba80067fa6f14f4648e4567ae4954e96f267
giwi97 <giwanthawijekoon@gmail.com>
2020-06-20 02:14:18 +0530

    Deleted previous code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
-function App() {
+
+class App extends Component {
+  state={isSignedIn:false}
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {this.state.isSignedIn ?
+        <text>Signed in!</text>
+        :
+        <text>Not signed in</text>
+      }
     </div>
   );
+  }
 }
 
 export default App;
