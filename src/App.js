d3a2b356b93891ffaa249366dea15e09d693e5f9
giwi97 <giwanthawijekoon@gmail.com>
2020-06-21 00:53:53 +0530

    App initialized with api key and domain authentication

--- src/App.js
+++ src/App.js
@@ -25,6 +25,11 @@ class App extends Component {
 
   componentDidMount = () => {
 
+    firebase.initializeApp({
+      apiKey: "AIzaSyDDgIwSGrwpmHF_RPupSPKtbhgtb289cY8",
+      authDomain: "task-planner-b7636.firebaseapp.com"
+    })
+
     firebase.auth().onAuthStateChanged(user => {
 
       this.setState({isSignedIn: !!user})
