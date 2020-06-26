dc559e8aab4585ceb6d6922adeb233cdb4774f3b
giwi97 <giwanthawijekoon@gmail.com>
2020-06-27 02:12:58 +0530

    sidebar implementation

--- src/components/layouts/sidebar.js
+++ src/components/layouts/sidebar.js
@@ -1,3 +1,32 @@
-import React from 'react'
+import React from "react";
+import {
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar,
+  FaChevronDown,
+} from "react-icons/fa";
 
-export const Sidebar = () => <p>Sidebar</p>
+export const Sidebar = () => (
+  <div className="sidebar" data-testid="sidebar">
+    <ul className="sidebar__generic">
+      <li>
+        <span>
+          <FaInbox />
+        </span>
+        <span>Alerts</span>
+      </li>
+      <li>
+        <span>
+          <FaRegCalendar />
+        </span>
+        <span>Today</span>
+      </li>
+      <li>
+        <span>
+          <FaRegCalendarAlt />
+        </span>
+        <span>Upcoming</span>
+      </li>
+    </ul>
+  </div>
+);
