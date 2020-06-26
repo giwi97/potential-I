2901cbce1a5659e9ceef5e52cd8f76dc71917881
giwi97 <giwanthawijekoon@gmail.com>
2020-06-27 02:58:01 +0530

    firebase configurations

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,15 @@
-import firebase from "firebase";
+import firebase from "firebase/app"
+import 'firebase/firestore'
 
 const firebaseConfiguration = firebase.initializeApp({
     apiKey: "AIzaSyDDgIwSGrwpmHF_RPupSPKtbhgtb289cY8",
     authDomain: "task-planner-b7636.firebaseapp.com",
+    databaseURL: "https://task-planner-b7636.firebaseio.com",
+    projectId: "task-planner-b7636",
+    storageBucket: "task-planner-b7636.appspot.com",
+    messagingSenderId: "483887023396",
+    appId: "1:483887023396:web:2145500a7c2a76b71c5fb1",
+    measurementId: "G-T4715DMWM7"
   });
 
   export { firebaseConfiguration as firebase }
