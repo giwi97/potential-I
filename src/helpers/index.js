19e39c9bec8b09e8c330d0ae64edfdf603a2035e
giwi97 <giwanthawijekoon@gmail.com>
2020-06-28 02:24:08 +0530

    helper for hook

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,4 @@
-import { collatedTasksExist } from '../constants'
+import { collatedTasks } from '../constants'
 
 export const collatedTasksExist = selectedProject => 
-collatedTasksExist.find(task => task.key)
+collatedTasks.find(task => task.key === selectedProject);
